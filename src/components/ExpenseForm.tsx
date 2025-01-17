9b9fee0d14c9d85c7e54625e1c6942586fb0221d
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-17 10:17:04 -0600

    creando formulario para registrar un gasto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,63 @@
+import { categories } from "../data/categories";
+
 export default function ExpenseForm() {
   return (
-    <form action="">holi</form>
+    <form action="" className="space-y-5">
+      <legend className="uppercase text-center text-2xl font-black border-b-4 py-2 border-blue-500">Nuevo Gasto</legend>
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="expenseName"
+          className="text-xl"
+        >Nombre Gasto: </label>
+        <input 
+          type="text"
+          id = "expenseName"
+           placeholder="Añade el nombre del gasto"
+           className="bg-slate-100 p-2"
+           name="expenseName"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="amount"
+          className="text-xl"
+        >Cantidad: </label>
+        <input 
+          type="number"
+          id = "amount"
+           placeholder="Añade la cantidad del gasto: ej. 200"
+           className="bg-slate-100 p-2"
+           name="amount"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="expenseAmount"
+          className="text-xl"
+        >Categoria: </label>
+        <select
+          id = "category"
+          className="bg-slate-100 p-2"
+          name="category"
+        >
+          <option value="" disabled>-- Seleccione --</option>
+          {categories.map(category =>(
+            <option
+              key={category.id}
+              value= {category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input 
+        type="submit"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white uppercase rounded-lg font-bold" 
+        value={'Registrar Gasto'}
+      />
+    </form>
   )
 }
+
