657ca1dc7011fbef1036376240eb3af25ab22ea9
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-23 11:54:34 -0600

    haciendo dinamico nuestro BudgetTracer

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -17,13 +17,14 @@ export default function ExpenseForm() {
   })
 
   const [error, setError] = useState('')
-  const {dispatch, state} = useBudget()
+  const [previousAmount, setPreviousAmount] = useState(0)
+  const {dispatch, state, remainingBudget} = useBudget()
 
 useEffect(() => {
    if(state.editingId){  
    const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]    
     setExpense(editingExpense)
-    
+    setPreviousAmount(editingExpense.amount)    
   }
     
 }, [state.editingId])
@@ -56,6 +57,12 @@ useEffect(() => {
       return
     }
     
+    //validar que no me pase del limite
+    if((expense.amount - previousAmount) > remainingBudget){
+      setError('Ese gasto se sale del presupuesto')
+      return
+    }
+
     //agregar o actualizar el gasto
     if(state.editingId){
       dispatch({type: 'update-expense', payload:{expense: {id: state.editingId, ...expense }}})
