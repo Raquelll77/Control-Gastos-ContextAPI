d45d07660855090e695c63ec39ad2fe7313b46d5
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-24 08:54:57 -0600

    afinando detalles del responsive

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -7,6 +7,6 @@ export const categories : Category[] = [
     { id: '4', name: 'Gastos Varios', icon: 'gastos' },
     { id: '5', name: 'Ocio', icon: 'ocio' },
     { id: '6', name: 'Salud', icon: 'salud' },
-    { id: '7', name: 'Suscripciones', icon: 'suscripciones' },
+    { id: '7', name: 'Suscripcion', icon: 'suscripciones' },
   ];
 
